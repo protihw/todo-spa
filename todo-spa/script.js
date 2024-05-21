b1836ea8258b700bdb842bd9a4d651be0bbe134f
proti <69305991+protihw@users.noreply.github.com>
2024-05-21 20:32:32 -0300

    Add files via upload

--- todo-spa/script.js
+++ todo-spa/script.js
@@ -1,17 +1,64 @@
-function addTask() {
+let taskList = [];
+
+function addTask(event) {
+    event.preventDefault()
+
     let description = document.getElementById("description");
 
     if (description.value == "") {
-        let alert = document.getElementById("alert");
-        let message_type = document.getElementById("message_type");
-        message_type.innerText = "Erro: "
-        let message = document.getElementById("message");
-        message.innerText = "Você precisa descrever a nova tarefa.";
-        alert.style.display = "block";
+        showMessage();
+    } else {
+        taskList.push(description.value)
+        description.value = "";
+        updateTasks();
     }
 }
 
 function closeMessage() {
     let alert = document.getElementById("alert");
     alert.style.display = "none";
+}
+
+function showMessage() {
+    let messageType = document.getElementById("message_type");
+    message_type.innerText = "Erro: "
+
+    let message = document.getElementById("message");
+    message.innerText = "Você precisa descrever a nova tarefa.";
+
+    let alert = document.getElementById("alert");
+    alert.style.display = "block";
+
+    setTimeout(() => {
+        closeMessage();
+    }, 4000);
+}
+
+function updateTasks() {
+    let divTasks = document.getElementById("tasks");
+
+    if (taskList.length > 0) {
+        let newOl = document.createElement("ol");
+
+        taskList.forEach((task) => {
+            let newLi = document.createElement("li");
+            newLi.innerText = task;
+            newOl.append(newLi);
+        })
+
+        divTasks.replaceChildren(newOl);
+    } else {
+        let p = document.createElement("p");
+        p.innerText = "Insira a primeira tarefa para começar...";
+        divTasks.replaceChildren(p);
+    }
+}
+
+function removeAll() {
+    taskList = [];
+    updateTasks();
+}
+
+function removeTask() {
+    
 }
