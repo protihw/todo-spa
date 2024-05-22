75fa81267562ddb9d7ed1e79138d65b740282cf2
proti <69305991+protihw@users.noreply.github.com>
2024-05-22 20:33:15 -0300

    Add files via upload

--- todo-spa/script.js
+++ todo-spa/script.js
@@ -39,14 +39,33 @@ function updateTasks() {
 
     if (taskList.length > 0) {
         let newOl = document.createElement("ol");
+        newOl.id = "tasks_box";
 
         taskList.forEach((task) => {
             let newLi = document.createElement("li");
             newLi.innerText = task;
+            
+            let checkBtn = document.createElement("a");
+            checkBtn.innerHTML = "Confirmar";
+            checkBtn.onclick = function () {
+                checkTask();
+            }
+
+            let removeBtn = document.createElement("a");
+            removeBtn.innerText = "Remover";
+            removeBtn.onclick = function () {
+                removeTask(task);
+            };
+
+            newLi.append(checkBtn);
+            newLi.append(removeBtn);
             newOl.append(newLi);
         })
 
         divTasks.replaceChildren(newOl);
+
+        let rdmBtn = document.getElementById("rdmbtn");
+        rdmBtn.disabled = false;
     } else {
         let p = document.createElement("p");
         p.innerText = "Insira a primeira tarefa para começar...";
@@ -59,6 +78,33 @@ function removeAll() {
     updateTasks();
 }
 
-function removeTask() {
+function removeTask(task) {
+    let taskIndex = taskList.indexOf(task);
+    taskList.splice(taskIndex, 1);
+    updateTasks();
+}
+
+
+// Terminar função
+
+function checkTask(task) {
+    let taskIndex = taskList.indexOf(task);
     
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function showTask(task) {
+    let newP = task;
+    let divTasks = document.getElementById("tasks");
+    divTasks.replaceChildren(newP);
+}
+
+function randomizeTask() {
+    randomIndex = getRandomInt(0, taskList.length);
+    showTask(taskList[randomIndex]);
 }
